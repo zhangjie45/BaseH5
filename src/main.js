36ae8ada49c8fe04b2eb3e8867c22d0d40c1239b
shian45 <shian45@noreply.gitcode.com>
2025-11-14 15:42:25 +0800

    修改uv-ui的引用方式

--- src/main.js
+++ src/main.js
@@ -1,14 +1,12 @@
 import {
-  createSSRApp
+    createSSRApp
 } from "vue";
 import App from "./App.vue";
-import uvUI from '@climblee/uv-ui'
 import { setupStore } from './store'
 export function createApp () {
-  const app = createSSRApp(App);
-  app.use(uvUI);
-  setupStore(app)
-  return {
-    app,
-  };
+    const app = createSSRApp(App);
+    setupStore(app)
+    return {
+        app,
+    };
 }
