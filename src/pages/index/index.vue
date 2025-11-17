a268e6015f0383c758a64a6e1a3f02f5a1a1fcee
shian45 <1109446910@qq.com>
2025-11-17 15:46:52 +0800

    引入wot_ui框架

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,64 +1,64 @@
 <template>
-  <view class="content">
-    <image class="logo" src="/static/logo.png"></image>
-    <view class="text-area">
-      <text class="title">{{ title }}</text>
-      <button type="primary" @click="useCountStore.count++"> 点击 {{ count }} </button>
-      <button @click="testReq">测试</button>
+    <view class="content">
+        <image class="logo" src="/static/logo.png"></image>
+        <view class="text-area">
+            <text class="title">{{ title }}</text>
+            <button type="primary" @click="useCountStore.count++"> 点击 {{ count }} </button>
+            <button @click="testReq">测试</button>
+        </view>
+        <button @click="testSetToken">测试</button>
+        <wd-button @click="jumpUrl">主要按钮</wd-button>
     </view>
-    <button @click="testSetToken">测试</button>
-    <uv-button type="primary" @click="jumpUrl" text="确定"></uv-button>
-  </view>
 </template>
 
 <script setup>
-  import { reqDictData } from '@/api/test'
-  import { SET_TOKEN, GET_TOKEN } from '@/utils/token'
-  const title = 'Hello Vue 3.0 + Vite'
-  import { useOutsideCountStore } from '@/store/count'
-  const useCountStore = useOutsideCountStore()
-  const count = computed(() => useCountStore.count)
+import { reqDictData } from '@/api/test'
+import { SET_TOKEN, GET_TOKEN } from '@/utils/token'
+const title = 'Hello Vue 3.0 + Vite'
+import { useOutsideCountStore } from '@/store/count'
+const useCountStore = useOutsideCountStore()
+const count = computed(() => useCountStore.count)
 
-  function testReq() {
+function testReq () {
     reqDictData().then((res) => {
-      console.log(res)
+        console.log(res)
     })
-  }
-  function testSetToken() {
+}
+function testSetToken () {
     SET_TOKEN('123')
     console.log(GET_TOKEN())
-  }
-  function jumpUrl() {
+}
+function jumpUrl () {
     uni.navigateTo({
-      url: '/pages/home'
+        url: '/pages/home'
     })
-  }
+}
 </script>
 
 <style>
-  .content {
+.content {
     display: flex;
     flex-direction: column;
     align-items: center;
     justify-content: center;
-  }
+}
 
-  .logo {
+.logo {
     height: 200rpx;
     width: 200rpx;
     margin-top: 200rpx;
     margin-left: auto;
     margin-right: auto;
     margin-bottom: 50rpx;
-  }
+}
 
-  .text-area {
+.text-area {
     display: flex;
     justify-content: center;
-  }
+}
 
-  .title {
+.title {
     font-size: 36rpx;
     color: #8f8f94;
-  }
+}
 </style>
